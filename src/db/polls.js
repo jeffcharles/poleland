f1e11ddba8b2a0cdda7162ba0b0228536f6969ec
Jeffrey Charles <jeffreycharles@gmail.com>
2014-01-01 00:20:33 -0500

    Add some in-memory data

--- src/db/polls.js
+++ src/db/polls.js
@@ -1,6 +1,75 @@
 /* global exports */
-var polls = [];
+var polls = [
+    {
+        id: '1',
+        questions: ['1', '2']
+    }, {
+        id: '2',
+        questions: ['3']
+    }
+];
+
+var questions = [
+    {
+        id: '1',
+        content: 'What do you think of foo?',
+        answers: ['1', '2', '3']
+    }, {
+        id: '2',
+        content: 'What is 2 + 3?',
+        answers: ['4', '5', '6']
+    }, {
+        id: '3',
+        content: 'Is this awesome?',
+        answers: ['7', '8']
+    }
+];
+
+var answers = [
+    {
+        id: '1',
+        content: 'I like it!'
+    }, {
+        id: '2',
+        content: 'I\'m indifferent'
+    }, {
+        id: '3',
+        content: 'I hate it!'
+    }, {
+        id: '4',
+        content: '4'
+    }, {
+        id: '5',
+        content: '5'
+    }, {
+        id: '6',
+        content: '6'
+    }, {
+        id: '7',
+        content: 'Yes'
+    }, {
+        id: '8',
+        content: 'No'
+    }
+];
 
 exports.getPolls = function() {
-    return polls;
+    var clone = function(x) { return JSON.parse(JSON.stringify(x)); };
+    return polls.map(function(poll) {
+        poll = clone(poll);
+        var mappedQuestions = poll.questions.map(function(questionId) {
+            var question = clone(questions.filter(function(question) {
+                return question.id == questionId;
+            })[0]);
+            var mappedAnswers = question.answers.map(function(answerId) {
+                return answers.filter(function(answer) {
+                    return answer.id == answerId;
+                })[0];
+            });
+            question.answers = mappedAnswers;
+            return question;
+        });
+        poll.questions = mappedQuestions;
+        return poll;
+    });
 };
--- test/acceptance/routes/polls.js
+++ test/acceptance/routes/polls.js
@@ -3,11 +3,11 @@ var request = require('supertest');
 var app = require('./../../../src/app');
 
 describe('GET /polls', function() {
-    it('returns an empty list of polls', function(done) {
+    it('returns a list of polls', function(done) {
         request(app)
             .get('/polls')
             .set('Accept', 'application/json')
             .expect('Content-Type', 'application/json')
-            .expect(200, '[]', done);
+            .expect(200, done);
     });
 });
