928bc0562bbec51e67704b123ff5ad4e97b36799
Jeffrey Charles <jeffreycharles@gmail.com>
2014-03-28 21:19:45 -0400

    Set up client-side routing

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -1,4 +1,12 @@
 /* global angular */
 angular.module('poleland', [
+    'ngRoute',
     'poleland.controllers'
-]);
+]).
+    config(['$routeProvider', function($routeProvider) {
+        $routeProvider.when('/polls', {
+            templateUrl: 'partials/polls.html',
+            controller: 'ListPolls'
+        });
+        $routeProvider.otherwise({redirectTo: '/polls'});
+    }]);
