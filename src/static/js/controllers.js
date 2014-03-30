9fc0ce3f0168f9710be001418582882ede794780
Jeffrey Charles <jeffreycharles@gmail.com>
2014-03-29 23:31:56 -0400

    Use more logging instead of outputting directly

--- src/static/js/controllers.js
+++ src/static/js/controllers.js
@@ -1,28 +1,23 @@
 /* global angular */
 angular.module('poleland.controllers', []).
-    controller('ListPolls', ['$scope', 'polls', function($scope, polls) {
-        $scope.message = 'Loading...';
-        $scope.polls = [];
-        polls.getPolls().
-            then(function(polls) {
-                $scope.message = '';
-                $scope.polls = polls;
-            }, function(err) {
-                switch(err) {
-                case 'ApiServerDown':
-                    $scope.message = 'API server down';
-                    break;
-                default:
-                    $scope.message = err;
-                }
-            });
-    }]).
+    controller('ListPolls',
+               ['$scope', '$log', 'polls', function($scope, $log, polls) {
+                   $scope.polls = [];
+                   polls.getPolls().
+                       then(function(polls) {
+                           $scope.polls = polls;
+                       }, function(err) {
+                           $log.error(err);
+                       });
+               }]).
     controller('Poll',
                ['$scope', '$location', '$log', 'polls', '$routeParams',
                 function($scope, $location, $log, polls, $routeParams) {
                     polls.getPoll($routeParams.pollHref).
                         then(function(poll) {
                             $scope.poll = poll;
+                        }, function(err) {
+                            $log.error(err);
                         });
 
                     $scope.deletePoll = function() {
