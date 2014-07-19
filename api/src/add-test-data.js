7675da7b420b42411c20e216e809c6ad5f28860a
Jeffrey Charles <jeffreycharles@gmail.com>
2014-07-19 17:34:17 -0400

    Create simple Node app for adding test data

---
+++ api/src/add-test-data.js
@@ -0,0 +1,82 @@
+/* global console, process, require */
+'use strict';
+var couchbase = require('couchbase');
+
+var polls = {
+    '1': {
+        _type: 'poll',
+        title: 'Random stuff',
+        questions: [
+            {
+                id: '1',
+                content: 'What do you think of foo?',
+                answers: [
+                    {
+                        id: '1',
+                        content: 'I like it!'
+                    },
+                    {
+                        id: '2',
+                        content: 'I\'m indifferent'
+                    },
+                    {
+                        id: '3',
+                        content: 'I hate it!'
+                    }
+                ]
+            },
+            {
+                id: '2',
+                content: 'What is 2 + 3?',
+                answers: [
+                    {
+                        id: '1',
+                        content: '4'
+                    },
+                    {
+                        id: '2',
+                        content: '5'
+                    },
+                    {
+                        id: '3',
+                        content: '6'
+                    }
+                ]
+            }
+        ]
+    },
+    '2': {
+        _type: 'poll',
+        title: 'Awesomeness measurements',
+        questions: [
+            {
+                id: '1',
+                content: 'Is this awesome?',
+                answers: [
+                    {
+                        id: '1',
+                        content: 'Yes'
+                    },
+                    {
+                        id: '2',
+                        content: 'No'
+                    }
+                ]
+            }
+        ]
+    }
+};
+
+var db =
+        new couchbase.Connection({host: '10.0.0.2:8091', bucket: 'poleland'});
+
+var pollsForCb = {};
+for(var pollId in polls) {
+    pollsForCb['polls/' + pollId] = { value: polls[pollId] };
+}
+db.setMulti(pollsForCb, null, function(err) {
+    if(err) {
+        console.log(err);
+    }
+    process.exit(err ? 1 : 0);
+});
