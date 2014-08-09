6b66e8a1985fbaf9dc0d794aeb839bc7a265e2c8
Jeffrey Charles <jeffreycharles@gmail.com>
2014-08-08 23:32:35 -0400

    Make poll tests re-runnable

--- api/test/acceptance/routes/polls.js
+++ api/test/acceptance/routes/polls.js
@@ -29,6 +29,41 @@ var poll = {
     ]
 };
 
+function createPoll(next) {
+    request(app)
+        .post('/api/v1/polls')
+        .set('Accept', 'application/json')
+        .set('Content-Type', 'application/json')
+        .send(poll)
+        .expect(201)
+        .end(function(err, res) {
+            assert.ifError(err);
+            var relativeUrl =
+                    '/' + res.header.location.split('/').slice(3).join('/');
+            next(relativeUrl);
+        });
+}
+
+function deletePoll(relativeUrl, next) {
+    request(app)
+        .delete(relativeUrl)
+        .expect(204)
+        .end(function(err) {
+            assert.ifError(err);
+            next();
+        });
+}
+
+function createTemporaryPoll(func, next) {
+    createPoll(function(pollRelativeUrl) {
+        func(pollRelativeUrl, function(err) {
+            deletePoll(pollRelativeUrl, function() {
+                next(err);
+            });
+        });
+    });
+}
+
 before(function() {
     appContainer.start();
 });
@@ -71,7 +106,7 @@ describe('POST /api/v1/polls', function() {
                         var resPoll = res.body;
                         delete resPoll._links;
                         assert.deepEqual(resPoll, poll);
-                        done();
+                        deletePoll(relativeUrl, done);
                     });
             });
     });
@@ -88,30 +123,35 @@ describe('POST /api/v1/polls', function() {
 
 describe('GET /api/v1/polls', function() {
     it('returns a list of polls', function(done) {
-        request(app)
-            .get('/api/v1/polls')
-            .set('Accept', 'application/json')
-            .expect('Content-Type', 'application/json; charset=utf-8')
-            .expect(200, done);
+        createTemporaryPoll(function(pollRelativeUrl, next) {
+            request(app)
+                .get('/api/v1/polls')
+                .set('Accept', 'application/json')
+                .expect('Content-Type', 'application/json; charset=utf-8')
+                .expect(200, next);
+        }, done);
     });
 });
 
 describe('GET /api/v1/polls/:id', function() {
     it('returns a poll when it exists', function(done) {
-        request(app)
-            .get('/api/v1/polls/1')
-            .set('Accept', 'application/json')
-            .expect('Content-Type', 'application/json; charset=utf-8')
-            .expect(200)
-            .end(function(err, res) {
-                if(err) {
-                    done(err);
-                    return;
-                }
-                assert.ok(
-                    res.body._links.self.href.match(/\/api\/v1\/polls\/1/));
-                done();
-            });
+        createTemporaryPoll(function(pollRelativeUrl, next) {
+            request(app)
+                .get(pollRelativeUrl)
+                .set('Accept', 'application/json')
+                .expect('Content-Type', 'application/json; charset=utf-8')
+                .expect(200)
+                .end(function(err, res) {
+                    if(err) {
+                        next(err);
+                        return;
+                    }
+                    assert.ok(
+                        res.body._links.self.href.indexOf(
+                            pollRelativeUrl) > -1);
+                    next();
+                });
+        }, done);
     });
     it('returns a 404 when it does not exist', function(done) {
         request(app)
@@ -123,32 +163,35 @@ describe('GET /api/v1/polls/:id', function() {
 
 describe('PUT /api/v1/polls/:id', function() {
     it('updates a poll when it exists', function(done) {
-        request(app)
-            .put('/api/v1/polls/1')
-            .set('Content-Type', 'application/json; charset=utf-8')
-            .send(poll)
-            .expect(204)
-            .end(function(err) {
-                if(err) {
-                    done(err);
-                    return;
-                }
-                request(app)
-                    .get('/api/v1/polls/1')
-                    .set('Accept', 'application/json')
-                    .expect('Content-Type', 'application/json; charset=utf-8')
-                    .expect(200)
-                    .end(function(err, res) {
-                        if(err) {
-                            done(err);
-                            return;
-                        }
-                        var resPoll = res.body;
-                        delete resPoll._links;
-                        assert.deepEqual(resPoll, poll);
-                        done();
-                    });
-            });
+        createTemporaryPoll(function(pollRelativeUrl, next) {
+            request(app)
+                .put(pollRelativeUrl)
+                .set('Content-Type', 'application/json; charset=utf-8')
+                .send(poll)
+                .expect(204)
+                .end(function(err) {
+                    if(err) {
+                        next(err);
+                        return;
+                    }
+                    request(app)
+                        .get(pollRelativeUrl)
+                        .set('Accept', 'application/json')
+                        .expect('Content-Type',
+                                'application/json; charset=utf-8')
+                        .expect(200)
+                        .end(function(err, res) {
+                            if(err) {
+                                next(err);
+                                return;
+                            }
+                            var resPoll = res.body;
+                            delete resPoll._links;
+                            assert.deepEqual(resPoll, poll);
+                            next();
+                        });
+                });
+        }, done);
     });
     it('returns a 404 when it does not exist', function(done) {
         request(app)
@@ -158,30 +201,35 @@ describe('PUT /api/v1/polls/:id', function() {
             .expect(404, done);
     });
     it('validates the poll', function(done) {
-        request(app)
-            .put('/api/v1/polls/1')
-            .set('Accept', 'application/json')
-            .set('Content-Type', 'application/json')
-            .send({ foo: 'bar' })
-            .expect(400)
-            .expect('Content-Type', 'application/json; charset=utf-8', done);
+        createTemporaryPoll(function(pollRelativeUrl, next) {
+            request(app)
+                .put(pollRelativeUrl)
+                .set('Accept', 'application/json')
+                .set('Content-Type', 'application/json')
+                .send({ foo: 'bar' })
+                .expect(400)
+                .expect('Content-Type', 'application/json; charset=utf-8',
+                        next);
+        }, done);
     });
 });
 
 describe('DELETE /polls/:id', function() {
     it('deletes a poll when it exists', function(done) {
-        request(app)
-            .del('/api/v1/polls/1')
-            .expect(204)
-            .end(function(err) {
-                if(err) {
-                    done(err);
-                    return;
-                }
-                request(app)
-                    .get('/api/v1/polls/1')
-                    .expect(404, done);
-            });
+        createPoll(function(pollRelativeUrl) {
+            request(app)
+                .del(pollRelativeUrl)
+                .expect(204)
+                .end(function(err) {
+                    if(err) {
+                        done(err);
+                        return;
+                    }
+                    request(app)
+                        .get(pollRelativeUrl)
+                        .expect(404, done);
+                });
+        });
     });
     it('returns a 404 when it does not exist', function(done) {
         request(app)
