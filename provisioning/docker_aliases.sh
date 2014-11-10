4b4f0b24a148e2978540968ecd0b97a192890bc9
Jeffrey Charles <jeffreycharles@gmail.com>
2014-11-09 20:42:06 -0500

    Cleanup containers when done

--- provisioning/docker_aliases.sh
+++ provisioning/docker_aliases.sh
@@ -4,4 +4,4 @@ alias do-node="docker run -v /vagrant:/usr/src/node --net=host \
     -e 'AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID' \
     -e 'AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY' \
     -e 'DYNAMO_DB_PREFIX=$DYNAMO_DB_PREFIX' \
-    -it node:0.10.28"
+    --rm -it node:0.10.28"
