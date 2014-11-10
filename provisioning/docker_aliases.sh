81ce9bb93ba2b5622e34bd9312adc5b9eec73204
Jeffrey Charles <jeffreycharles@gmail.com>
2014-11-09 20:37:53 -0500

    Allow running Docker as non-root user

--- provisioning/docker_aliases.sh
+++ provisioning/docker_aliases.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-alias do-node="sudo docker run -v /vagrant:/usr/src/node --net=host \
+alias do-node="docker run -v /vagrant:/usr/src/node --net=host \
     -e 'AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID' \
     -e 'AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY' \
     -e 'DYNAMO_DB_PREFIX=$DYNAMO_DB_PREFIX' \
